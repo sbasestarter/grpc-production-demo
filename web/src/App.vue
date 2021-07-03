320614978bf84b9f3f3ff0450692770dba3345d0
zhaojiuzhou <zhaojiuzhou@guangtuikeji.com>
2021-07-03 10:51:32 +0800

    npm i vue-i18n -S
    多语言支持

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button @click="langSwitch">el-button</el-button>
+    <p>{{ $t("test") }}</p>
+    <el-input type="textarea" :rows="20" :placeholder="testText()" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld,
-  },
-};
+  methods: {
+    testText() {
+      return this.$i18n.t("test");
+    },
+    langSwitch() {
+      if (this.$i18n.locale === "zh_CN") {
+        this.$i18n.locale = "en_US";
+      } else {
+        this.$i18n.locale = "zh_CN";
+      }
+    }
+  }
+}
+
 </script>
 
 <style>
